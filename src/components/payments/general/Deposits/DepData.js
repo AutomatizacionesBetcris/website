f2bd701f17aac265f3200629473bf7dd6ff9d882
AutomatizacionesBetcris <mobando@betcris.com>
2023-04-12 17:35:04 -0700

    Se termina primer diseno de dep generales

--- src/components/payments/general/Deposits/DepData.js
+++ src/components/payments/general/Deposits/DepData.js
@@ -2,11 +2,24 @@ import Pay4Fun from "../../../../img/payments/Pay4fun.jpg";
 import Pix from "../../../../img/payments/Pix.jpg";
 import EcoPayz from "../../../../img/payments/Ecopayz.jpg";
 import Tarjetas from "../../../../img/payments/Tarjetas.jpg";
+import panama from "../../../../img/flags/panama.png";
+import ecuador from "../../../../img/flags/ecuador.png";
+import guatemala from "../../../../img/flags/guatemala.png";
+import honduras from "../../../../img/flags/honduras.png";
+import peru from "../../../../img/flags/peru.png";
+import repdominicana from "../../../../img/flags/republica-dominicana.png";
+import nicaragua from "../../../../img/flags/nicaragua.png";
+import mexico from "../../../../img/flags/mexico.png";
+import elsalvador from "../../../../img/flags/el-salvador.png";
+import chile from "../../../../img/flags/chile.png";
+import brazil from "../../../../img/flags/brasil.png";
+import canada from "../../../../img/flags/canada.png";
 
 // Info en ingles
 const depositDataEn = [
   {
     pais: "Panama",
+    flag: panama,
     info: [
       {
         img: EcoPayz,
@@ -47,6 +60,7 @@ const depositDataEn = [
   },
   {
     pais: "Ecuador",
+    flag: ecuador,
     info: [
       {
         img: Tarjetas,
@@ -73,6 +87,7 @@ const depositDataEn = [
   },
   {
     pais: "Guatemala",
+    flag: guatemala,
     info: [
       {
         img: Tarjetas,
@@ -99,6 +114,7 @@ const depositDataEn = [
   },
   {
     pais: "Honduras",
+    flag: honduras,
     info: [
       {
         img: Tarjetas,
@@ -124,7 +140,42 @@ const depositDataEn = [
     ],
   },
   {
+    pais: "Peru",
+    flag: peru,
+    info: [
+      {
+        img: Tarjetas,
+        title: "PIX",
+        url: "https://br.betcris.help/hc/pt-br/articles/360018482278-PIX",
+      },
+
+      {
+        img: EcoPayz,
+        title: "PAY4FUN",
+        url: "https://br.betcris.help/hc/pt-br/articles/360011459957-Pay4Fun",
+      },
+    ],
+  },
+  {
+    pais: "Republica Dominicana",
+    flag: repdominicana,
+    info: [
+      {
+        img: Tarjetas,
+        title: "PIX",
+        url: "https://br.betcris.help/hc/pt-br/articles/360018482278-PIX",
+      },
+
+      {
+        img: EcoPayz,
+        title: "PAY4FUN",
+        url: "https://br.betcris.help/hc/pt-br/articles/360011459957-Pay4Fun",
+      },
+    ],
+  },
+  {
     pais: "Nicaragua",
+    flag: nicaragua,
     info: [
       {
         img: Tarjetas,
@@ -151,6 +202,7 @@ const depositDataEn = [
   },
   {
     pais: "Mexico",
+    flag: mexico,
     info: [
       {
         img: Tarjetas,
@@ -172,6 +224,74 @@ const depositDataEn = [
         img: Tarjetas,
         title: "PIX",
         url: "https://br.betcris.help/hc/pt-br/articles/360018482278-PIX",
+      },
+    ],
+  },
+  {
+    pais: "El Salvador",
+    flag: elsalvador,
+    info: [
+      {
+        img: Tarjetas,
+        title: "PIX",
+        url: "https://br.betcris.help/hc/pt-br/articles/360018482278-PIX",
+      },
+
+      {
+        img: EcoPayz,
+        title: "PAY4FUN",
+        url: "https://br.betcris.help/hc/pt-br/articles/360011459957-Pay4Fun",
+      },
+    ],
+  },
+  {
+    pais: "Chile",
+    flag: chile,
+    info: [
+      {
+        img: Tarjetas,
+        title: "PIX",
+        url: "https://br.betcris.help/hc/pt-br/articles/360018482278-PIX",
+      },
+
+      {
+        img: EcoPayz,
+        title: "PAY4FUN",
+        url: "https://br.betcris.help/hc/pt-br/articles/360011459957-Pay4Fun",
+      },
+    ],
+  },
+  {
+    pais: "Brazil",
+    flag: brazil,
+    info: [
+      {
+        img: Tarjetas,
+        title: "PIX",
+        url: "https://br.betcris.help/hc/pt-br/articles/360018482278-PIX",
+      },
+
+      {
+        img: EcoPayz,
+        title: "PAY4FUN",
+        url: "https://br.betcris.help/hc/pt-br/articles/360011459957-Pay4Fun",
+      },
+    ],
+  },
+  {
+    pais: "Canada",
+    flag: canada,
+    info: [
+      {
+        img: Tarjetas,
+        title: "PIX",
+        url: "https://br.betcris.help/hc/pt-br/articles/360018482278-PIX",
+      },
+
+      {
+        img: EcoPayz,
+        title: "PAY4FUN",
+        url: "https://br.betcris.help/hc/pt-br/articles/360011459957-Pay4Fun",
       },
     ],
   },
--- src/components/payments/general/Deposits/DepMenu.js
+++ src/components/payments/general/Deposits/DepMenu.js
@@ -27,16 +27,18 @@ const DepMenu = () => {
         {/* Seleccion de pais */}
         {selectedCountry === "" ? (
           <div>
-            <div className="text-base text-center mt-10">
-              <h1>Select a country</h1>
+            <div className="p-5 m-5 bg-sky-900 bg-opacity-20 rounded-2xl shadow-black shadow-sm">
+              <div className="text-sm lg:text-base text-white text-center">
+                <h1>SELECT A COUNTRY</h1>
+              </div>
             </div>
-            <div className="grid grid-cols-2 p-5 m-10 gap-5 justify-center items-center lg:m-40 lg:p-10 bg-sky-900 bg-opacity-20 rounded-2xl shadow-lg ">
+            <div className="grid grid-cols-3 p-5 m-5 gap-5 justify-center items-center lg:grid-cols-3 lg:m-6 lg:p-10 bg-sky-900 bg-opacity-20 rounded-2xl shadow-black shadow-md ">
               {depositData.map((country) => (
                 <div className="flex justify-center items-center">
                   <button
                     key={country.pais}
                     onClick={() => handleCountrySelect(country.pais)}
-                    className="border h-10 w-28 lg:h-16 lg:w-96 rounded-2xl"
+                    className="h-12 w-28 text-xs lg:text-base lg:h-16 lg:w-96 rounded-lg shadow-black shadow-sm bg-blue-600 text-white hover:bg-indigo-700"
                   >
                     {country.pais}
                   </button>
@@ -45,13 +47,39 @@ const DepMenu = () => {
             </div>
           </div>
         ) : (
-          <div className="text-base text-center mt-10">
-            <button
-              onClick={handleBackButtonClick}
-              className="border h-12 w-60 lg:h-16 lg:w-80 rounded-2xl"
-            >
-              Back to country selection
-            </button>
+          <div className="text-sm lg:text-base text-center mt-5">
+            <div className="p-5 m-5 bg-sky-900 bg-opacity-20 rounded-2xl shadow-black shadow-sm">
+              <div className="flex text-sm lg:text-base text-white justify-between">
+                <button
+                  onClick={handleBackButtonClick}
+                  className="flex w-24 items-center text-white hover:scale-110 transition-transform duration-300 ease-out hover:text-gray-300 hover:underline"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5 ml-2 mr-2"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M6.72 10l5.29 5.29a.75.75 0 0 1-1.06 1.06l-6-6a.75.75 0 0 1 0-1.06l6-6a.75.75 0 0 1 1.06 1.06L6.72 10z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  BACK
+                </button>
+
+                <img
+                  src={
+                    depositData.find(
+                      (country) => country.pais === selectedCountry
+                    )?.flag
+                  }
+                  alt={`Flag of ${selectedCountry}`}
+                  className="h-10 w-12 rounded-lg md:h-10 md:w-12 lg:h-12 lg:w-14"
+                />
+              </div>
+            </div>
           </div>
         )}
       </div>
