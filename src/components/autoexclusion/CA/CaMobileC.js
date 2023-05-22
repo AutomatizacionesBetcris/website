179cb56f0c8894f5288aad64f3c190afa8c2721d
AutomatizacionesBetcris <mobando@betcris.com>
2023-05-22 09:57:09 -0700

    Se agrega la traduccion a CA

--- src/components/autoexclusion/CA/CaMobileC.js
+++ src/components/autoexclusion/CA/CaMobileC.js
@@ -13,12 +13,12 @@ const CaMobileC = () => {
       <div className="grid lg:justify-center lg:items-center">
         <div className=" text-center p-2 m-5 bg-sky-900 bg-opacity-20 rounded-2xl shadow-lg ">
           <p>
-            <span className="text-white text-base font-bold">¡Recuerda!</span>
+            <span className="text-white text-base font-bold">Remember!</span>
           </p>
           <p>
             <span className="text-slate-200 text-xs text-center">
-              Una vez hecho este proceso no se podrá habilitar la cuenta hasta
-              que dicha Autoexclusión haya terminado.
+              Once you have done this process, you won't be able to enable your
+              account until the self-exclusion period has ended.
             </span>
           </p>
         </div>
@@ -32,39 +32,38 @@ const CaMobileC = () => {
 
         <div className=" text-slate-200 text-xs p-5 m-5 bg-sky-900 bg-opacity-20 rounded-2xl shadow-lg">
           <p className="mb-3">
-            <strong>Toma en cuenta lo siguiente:</strong>
+            <strong>Take into account the following:</strong>
             <ul>
               <li>
-                <strong>- </strong>Las autoexclusiones son irreversibles durante
-                el tiempo especificado.
+                <strong>- </strong>Self-exclusions are irreversible for the
+                specified time.
               </li>
               <li>
-                <strong>- </strong>Durante el período de autoexclusión, no
-                podrás utilizar tu cuenta para apostar y jugar, tu cuenta se
-                cerrará y se te devolverán los fondos de la cuenta.
+                <strong>- </strong>During the self-exclusion time you won't be
+                able to gamble or use your account; your account will be closed
+                and your funds will be sent back to your account.
               </li>
             </ul>
           </p>
           <p className="mb-3">
-            <strong>Sigue estos pasos:</strong>
+            <strong>Follow these steps:</strong>
             <ul>
               <li>
-                <strong>1- </strong>Ingresa a tu cuenta Betcris (el proceso se
-                hace dentro de tu cuenta) por lo tanto, debes tener acceso a la
-                misma.
+                <strong>1- </strong>Log in to your betcris account (the process
+                is done once you are in); therefore, you will need to have
+                access to it.
               </li>
               <li>
-                <strong>2- </strong>Una vez dentro de tu cuenta; desplazate
-                hacia abajo (parte inferior de la pantalla).
+                <strong>2- </strong>Scroll down to the footer of the screen.
               </li>
               <li>
-                <strong>3- </strong>Por último haz clic en 'Autoexclusión'.
+                <strong>3- </strong>Lastly, click on 'Self-exclusion'.
               </li>
             </ul>
           </p>
           <p className="mb-3">
-            Desde ahí podrás definir el tiempo que desees para cerrar tu cuenta
-            ya sea por unos meses o de manera 'indefinida'.
+            There, you will be able to set the time frame you want to close your
+            account, either just for a few months or indefinetely.
           </p>
           <button className=" text-center text-xs font-semibold w-full h-8 shadow-3xl shadow-black rounded-lg bg-green-600 hover:bg-green-700">
             <a
@@ -72,7 +71,7 @@ const CaMobileC = () => {
               target="_link"
               rel="noopener noreferrer"
             >
-              Ver más información aquí
+              More information here
             </a>
           </button>
         </div>
