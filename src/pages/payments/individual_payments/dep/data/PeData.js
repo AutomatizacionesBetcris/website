fbfb6dd5969ac29cdae29620b7e216cf33256eba
AutomatizacionesBetcris <mobando@betcris.com>
2023-06-22 17:43:58 -0700

    Se organiza DEP y RET PE

---
+++ src/pages/payments/individual_payments/dep/data/PeData.js
@@ -0,0 +1,189 @@
+import PaysafeCard from "../../../../../img/payments/PaysafeCard.jpg";
+import Tarjetas from "../../../../../img/payments/Tarjetas.jpg";
+import Agencias from "../../../../../img/payments/Betcris_Agencias.png";
+import PagoEfectivo from "../../../../../img/payments/PagoEfectivo.jpg";
+import SafetyPay from "../../../../../img/payments/SafetyPay_PE.jpg";
+import AstroPay from "../../../../../img/payments/Astropay.jpg";
+import Skrill from "../../../../../img/payments/Skrill.jpg";
+import Neteller from "../../../../../img/payments/Neteller.jpg";
+import EcoPayz from "../../../../../img/payments/Ecopayz.jpg";
+import Yape from "../../../../../img/payments/Yape.jpg";
+import Skrill1Tap from "../../../../../img/payments/Skril_tap.jpg";
+import Banktransfer from "../../../../../img/payments/Bank_transfer_PE_RET.jpg";
+
+const PeData = [
+  {
+    img: Tarjetas,
+    title: "DEPÓSITO TARJETAS DE CRÉDITO Y DÉBITO",
+    url: "https://get.betcris.help/hc/es-419/articles/360002208798-Tarjetas-de-cr%C3%A9dito-y-d%C3%A9bito",
+    min: "$10.00",
+    max: "$5,000.00",
+    time: "Inmediato",
+    descriptionTitle: "Desde el Cajero de Betcris:",
+    description: [
+      "Elige 'Depósito' → 'Tarjetas'",
+      "Ingresa la información solicitada si esta es la primera vez que estás usando una tarjeta (para acelerar el proceso, la próxima vez que realices un depósito, los datos de tu tarjeta ya estarán almacenados) y haz clic en 'Guardar'",
+      "Ingresa el monto que quieres depositar y tu número de identificación y haz clic en 'Siguiente'",
+      "Selecciona 'Confirmar' para completar la transacción. Los fondos serán acreditados de forma inmediata",
+    ],
+  },
+  {
+    img: PagoEfectivo,
+    title: "PAGO EFECTIVO",
+    url: "https://get.betcris.help/hc/es-419/articles/4566182496401-PagoEfectivo-",
+    min: "$10.00",
+    max: "$1,000.00",
+    time: "Inmediato",
+    descriptionTitle: "Desde el Cajero de Betcris:",
+    description: [
+      "Selecciona 'Depósito' → 'Banca móvil y agentes'",
+      "Ingresa el monto que quieres depositar y haz clic en 'Depositar'",
+      "Muestra el Código de Pago, el monto y la fecha de expiración que te proporcionan en un punto de pago para obtener el crédito en tu cuenta Betcris",
+    ],
+  },
+  {
+    img: PaysafeCard,
+    title: "PAYSAFE CARD",
+    url: "https://get.betcris.help/hc/es-419/articles/360002241357-paysafecard-",
+    min: "$5.00",
+    max: "$2,000.00",
+    time: "Inmediato",
+    descriptionTitle: "Desde el Cajero de Betcris:",
+    description: [
+      "Selecciona 'Depósito' → 'PaySafecard'",
+      "Ingresa el monto del depósito y haz clic en 'Depositar'",
+      "Ingresa el PIN de 16 dígitos de PaySafecard",
+      "Haz clic en 'Depositar' para completar la transacción",
+    ],
+  },
+  {
+    img: Skrill,
+    title: "SKRILL",
+    url: "https://get.betcris.help/hc/es-419/articles/360002218658-Skrill",
+    min: "$10.00",
+    max: "$95,000.00",
+    time: "Inmediato",
+    descriptionTitle: "Desde el Cajero de Betcris:",
+    description: [
+      "Selecciona 'Depósito' → 'Skrill'",
+      "Ingresa el monto, tu cuenta Skrill y haz clic en 'Depositar'",
+      "Completa la transacción en la página de Skrill que se te redirecciona",
+    ],
+  },
+  {
+    img: Neteller,
+    title: "NETELLER",
+    url: "https://ayuda.betcris.com/guia-de-usuario/centro-de-ayuda/depositos/peru/neteller-9/",
+    min: "$10.00",
+    max: "$95,000.00",
+    time: "Inmediato",
+    descriptionTitle: "Desde el Cajero de Betcris:",
+    description: [
+      "Selecciona 'Depósito' → 'NETELLER'",
+      "Ingresa el monto deseado, tu número de cuenta y haz clic en 'Depositar'",
+      "Completa la transacción en el sitio de NETELLER al que se te redirige",
+    ],
+  },
+  {
+    img: Skrill1Tap,
+    title: "SKRILL 1-TAP",
+    url: "https://get.betcris.help/hc/es-419/articles/360002218838-Skrill-1-Tap",
+    min: "$10.00",
+    max: "$95,000.00",
+    time: "Inmediato",
+    descriptionTitle: "Desde el Cajero de Betcris:",
+    description: [
+      "Selecciona 'Depósito' → 'Skrill 1-Tap'",
+      "Ingresa el monto, tu cuenta Skrill account y haz clic en 'Depositar'",
+      "Completa la transacción en la página de Skrill donde se te redirige",
+    ],
+  },
+  {
+    img: EcoPayz,
+    title: "ECOPAYZ",
+    url: "https://get.betcris.help/hc/es-419/articles/360002218938-ecoPayz",
+    min: "$10.00",
+    max: "$48,000.00",
+    time: "Inmediato",
+    descriptionTitle: "Desde el Cajero de Betcris:",
+    description: [
+      "Selecciona 'Depósito' → 'ecoPayz'",
+      "Ingresa el monto que quieres depositar, tu número de cuenta ecoPayz y haz clic en 'Confirmar'",
+      "Completa la transacción en el sitio de ecoPayz al que se te redirige",
+    ],
+  },
+  {
+    img: SafetyPay,
+    title: "SAFETYPAY",
+    url: "https://ayuda.betcris.com/guia-de-usuario/centro-de-ayuda/depositos/peru/safetypay-5/",
+    min: "$20.00",
+    max: "$8,000.00",
+    time: "60 minutos",
+    description: [
+      "Puedes usar SafetyPay (Pagoseguro) para hacer depósitos usando efectivo a través de múltiples puntos de cobro, como agencias bancarias o tiendas de conveniencia.",
+    ],
+  },
+  {
+    img: AstroPay,
+    title: "ASTROPAY",
+    url: "https://ayuda.betcris.com/guia-de-usuario/centro-de-ayuda/depositos/peru/astropay-card-9/",
+    min: "$6.00",
+    max: "$100,000.00",
+    time: "Inmediato",
+    descriptionTitle: "Desde el Cajero de Betcris:",
+    description: [
+      "Selecciona 'Depósito' → 'AstroPay'",
+      "Ingresa el monto de depósito y haz clic en 'Siguiente'",
+      "Continúa el proceso en la página que te redirige el sistema",
+      "Ingresa tu número de teléfono registrado en tu cuenta AstroPay",
+      "Elige el método de pago, ya sea un voucher ya adquirido o el saldo disponible en tu cuenta",
+      "Confirma tu depósito",
+    ],
+  },
+  {
+    img: Yape,
+    title: "YAPE",
+    url: "https://get.betcris.help/hc/es-419/articles/11446138940689-Yape",
+    min: "$4.00",
+    max: "$120.00",
+    time: "Inmediato",
+    descriptionTitle: "Desde el Cajero de Betcris:",
+    description: [
+      "Selecciona 'Depósito' → 'Yape o Plin'",
+      "Elige un método, ingresa el monto que deseas y haz clic en 'Depositar'",
+      "Escanea el código QR que se genera en tu pantalla desde tu aplicación favorita",
+    ],
+  },
+  {
+    img: Banktransfer,
+    title: "TRANSFERENCIA BANCARIA LOCAL",
+    url: "https://get.betcris.help/hc/es-419/articles/360002241437-Transferencias-bancarias",
+    min: "$10.00",
+    max: "$8,000.00",
+    time: "Una vez aprobados, los fondos se tramitan de forma inmediata a tu cuenta.",
+    descriptionTitle: "Desde el Cajero de Betcris:",
+    description: [
+      "Selecciona 'Depósito' → 'Transferencia Bancaria'",
+      "Elige el banco donde vas a depositar",
+      "Selecciona la cuenta y el destinatario para realizar el depósito",
+      "Completa los detalles requeridos y adjunta una fotografía del depósito realizado en el banco o en línea",
+      "Haz clic en 'Reportar depósito'",
+    ],
+  },
+  {
+    img: Agencias,
+    title: "AGENCIAS BETCRIS",
+    url: "https://get.betcris.help/hc/es-419/articles/6404360144273-Dep%C3%B3sitos-en-Agencias-Betcris",
+    min: "$",
+    max: "$",
+    time: "Inmediato",
+    descriptionTitle: "Dentro de quioscos o agencias Betcris:",
+    description: [
+      "Ofrecemos una variedad de formas rápidas y convenientes para depositar fondos en tu cuenta, incluyendo retiros en las Agencias oficiales de Betcris.",
+      "Para hacer tu depósito, solo debes presentar tu identificación y número de cuenta, junto con los fondos a depositar.",
+      "Contacta a tu Agencia Betcris más cercana para confirmar los montos mínimos y máximos por transacción, así como cualquier detalle adicional que necesites, ya que pueden variar según el país.",
+    ],
+  },
+];
+
+export default PeData;
